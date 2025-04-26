26e466ce27aa766ab79803150d92e400d431a5aa
Ilian Bronchart <ilianbronchart@protonmail.com>
2025-04-26 15:24:09 +0200

    writing proof of concept part of the thesis

--- src/api/controllers/glasses_controller.py
+++ src/api/controllers/glasses_controller.py
@@ -7,6 +7,8 @@ from src.db import Recording
 
 
 async def is_connected(glasses_hostname: str = DEFAULT_GLASSES_HOSTNAME) -> bool:
+    return True
+
     try:
 
         async def connect():
@@ -20,6 +22,7 @@ async def is_connected(glasses_hostname: str = DEFAULT_GLASSES_HOSTNAME) -> bool
 
 
 async def get_battery_level(glasses_hostname: str = DEFAULT_GLASSES_HOSTNAME) -> float:
+    return 82
     try:
         async with connect_to_glasses.with_hostname(
             glasses_hostname, using_ip=True
@@ -47,6 +50,49 @@ async def recording_from_glasses(glasses_recording: GlassesRecording) -> Recordi
 async def get_recordings(
     glasses_hostname: str = DEFAULT_GLASSES_HOSTNAME,
 ) -> list[Recording]:
+    return [
+        Recording(
+            uuid="946cffb5-a018-4b7a-bf67-ab7a172af12c",
+            visible_name="Test recording",
+            participant="Test participant",
+            created="2023-10-01T12:00:00",
+            duration="00:01:00",
+            folder_name="test_folder",
+            scene_video_url="https://example.com/scene_video.mp4",
+            gaze_data_url="https://example.com/gaze_data.csv",
+        ),
+        Recording(
+            uuid="3bd86d9b-06e1-40fa-b587-fdba559a344c",
+            visible_name="Another recording",
+            participant="Another participant",
+            created="2023-10-02T12:00:00",
+            duration="00:02:00",
+            folder_name="another_folder",
+            scene_video_url="https://example.com/another_scene_video.mp4",
+            gaze_data_url="https://example.com/another_gaze_data.csv",
+        ),
+        Recording(
+            uuid="39f5164f-873d-4d6b-be6b-e1d5db79c02a",
+            visible_name="Third recording",
+            participant="Third participant",
+            created="2023-10-03T12:00:00",
+            duration="00:03:00",
+            folder_name="third_folder",
+            scene_video_url="https://example.com/third_scene_video.mp4",
+            gaze_data_url="https://example.com/third_gaze_data.csv",
+        ),
+        Recording(
+            uuid="56f64c07-8066-4def-a965-df05616d56a6",
+            visible_name="Fourth recording",
+            participant="Fourth participant",
+            created="2023-10-04T12:00:00",
+            duration="00:04:00",
+            folder_name="fourth_folder",
+            scene_video_url="https://example.com/fourth_scene_video.mp4",
+            gaze_data_url="https://example.com/fourth_gaze_data.csv",
+        ),
+    ]
+
     """Retrieve metadata for all recordings on the glasses"""
     async with (
         connect_to_glasses.with_hostname(glasses_hostname, using_ip=True) as g3,
